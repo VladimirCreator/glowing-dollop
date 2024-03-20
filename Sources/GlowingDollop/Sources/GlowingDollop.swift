53f70bc8f485b0890080f9f6bc4e5747000a6aae
Vladimir <49140851+VladimirCreator@users.noreply.github.com>
2024-03-20 09:53:01 +0500

    chore: wip

--- Sources/GlowingDollop/Sources/GlowingDollop.swift
+++ Sources/GlowingDollop/Sources/GlowingDollop.swift
@@ -5,7 +5,7 @@ import GlowingDollopKit
 
 @main
 fileprivate struct GlowingDollop: App {
-    @StateObject private var searchViewModel: SearchViewModel = .init() async {
+    @StateObject private var searchViewModel: SearchViewModel = .init() {
         guard let url: URL = .init(string: "https://run.mocky.io/v3/ed41d10e-0c1f-4439-94fa-9702c9d95c14") else { fatalError() }
         async let (data, _) = URLSession.shared.data(from: url)
         
